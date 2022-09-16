5cd6e568794f1f20f00f10ec185bf787ef82d828
Luan Pires <luanmpires@gmail.com>
2022-09-16 00:13:20 -0300

    iniciando o mobile da aplicação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,26 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from '@expo-google-fonts/inter'
 
+
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading/index';
+import { Home } from './src/screens/Home';
 export default function App() {
+  const [fontsLoader] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Hello peplow 
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoader ? <Home/> : <Loading />}
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {  
-    color: '#fff',
-    fontSize: 22,
-  }  
-});
